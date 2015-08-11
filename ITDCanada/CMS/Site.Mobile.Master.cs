b29dde9913eebd3679ea2791f0c1efc2f055074e
koala0905 <bojiun.tseng@gmail.com>
2015-08-10 19:27:44 -0700

    add CMS project

--- ITDCanada/CMS/Site.Mobile.Master.cs
+++ ITDCanada/CMS/Site.Mobile.Master.cs
@@ -1,13 +1,13 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-namespace ITDC
+namespace CMS
 {
-    public partial class About : Page
+    public partial class Site_Mobile : System.Web.UI.MasterPage
     {
         protected void Page_Load(object sender, EventArgs e)
         {
